4089376dbcc3a29e7643ccec3aed5c3f52a42a19
mumat0103 <wsx2138@naver.com>
2022-04-11 00:00:18 +0900

    실습

---
+++ blog/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=20)
+    content = models.TextField()
+    author = models.CharField(max_length=4,default="***")
+    author_email = models.CharField(max_length=50, default="wsx2138@naver.com")
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.title
