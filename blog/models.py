36d2729e480e32b0644bdf8a76e9c32c52a52c6b
mumat0103 <wsx2138@naver.com>
2022-04-11 20:05:26 +0900

    ?

--- blog/models.py
+++ blog/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Blog(models.Model):
     title = models.CharField(max_length=20)
     content = models.TextField()
-    author = models.CharField(max_length=4,default="***")
+    author = models.CharField(max_length=4, default="***")
     author_email = models.CharField(max_length=50, default="wsx2138@naver.com")
     created_at = models.DateTimeField(auto_now_add=True)
     
