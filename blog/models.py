b21d3b0b9cb8a240554460db2ef7257bf24de010
mumat0103 <wsx2138@naver.com>
2022-04-11 22:28:15 +0900

    4/11 CRUD 세션

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-
 class Blog(models.Model):
     title = models.CharField(max_length=20)
     content = models.TextField()
@@ -10,3 +9,6 @@ class Blog(models.Model):
     
     def __str__(self):
         return self.title
+
+    def summary(self):
+        return self.content[:100]
